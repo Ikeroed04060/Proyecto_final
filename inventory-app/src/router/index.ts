ee698dd1f4db526fca3f356c52f2613be055caba
Catalina Cortes <accortes-2022b@corhuila.edu.co>
2024-11-16 23:02:14 -0500

    configuracion vista proveedores

--- inventory-app/src/router/index.ts
+++ inventory-app/src/router/index.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import HomePage from '../views/HomePage.vue';
 import ProductPage from '@/views/ProductPage.vue';
+import ProviderPage from '@/views/ProviderPage.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,8 +19,15 @@ const routes: Array<RouteRecordRaw> = [
   path: '/producto',
   name: 'Producto',
   component: ProductPage
+},
+
+{
+  path: '/proveedores',
+  name: 'Proveedores',
+  component: ProviderPage
 }
 ]
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
